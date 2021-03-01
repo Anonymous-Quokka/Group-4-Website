bf77e678fa56220016eeb7422373566bc862c9e6
Jason Blecman <jblecman@ucsc.edu>
2021-03-01 14:28:16 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,36 @@
-// Find the button by Id
-var button1 = document.getElementById("button1");
-var button2 = document.getElementById("button2");
-var button3 = document.getElementById("button3");
-var button4 = document.getElementById("button4");
+let clickNum = 0
+
+function countUp(num, val) {
+  let iter = 0;
+  for (let i = 0; i <= num; i += num/100) {
+    setTimeout(function() {
+      if (clickNum == val) {
+        $("#counter_bubble").html("<h1>" + Math.floor(i) + "<h1>");
+      }
+    }, iter*10);
+    iter += 1;
+  }
+  setTimeout(function() {
+    if (clickNum == val) {
+      $("#counter_bubble").html("<h1>" + Math.floor(num) + "<h1>");
+    }
+  }, 1000);
+}
+countUp(100);
 
 // Create an event listener attached to buttons
-button1.addEventListener("click", function() {
-  document.getElementById("counter_bubble").innerHTML = "<h1>567</h1>";
-})
-button2.addEventListener("click", function() {
-  document.getElementById("counter_bubble").innerHTML = "<h1>245</h1>";
-})
-button3.addEventListener("click", function() {
-  document.getElementById("counter_bubble").innerHTML = "<h1>30</h1>";
-})
-button4.addEventListener("click", function() {
-  document.getElementById("counter_bubble").innerHTML = "<h1>~10</h1>";
-})
+$("#button1").click(function() {
+  countUp(567, ++clickNum);
+});
+
+$("#button2").click(function() {
+  countUp(245, ++clickNum);
+});
+
+$("#button3").click(function() {
+  countUp(30, ++clickNum);
+});
+
+$("#button4").click(function() {
+  countUp(10, ++clickNum);
+});
