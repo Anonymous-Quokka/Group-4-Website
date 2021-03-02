175b91a77da085e4d253220bedb51db65275bea6
Jason Blecman <jblecman@ucsc.edu>
2021-03-01 23:20:31 -0800

    Spawning Added

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,31 @@
 let clickNum = 0
+const srcs = ["img/vaquita-purple.png", "img/vaquita-blue.png", "img/vaquita-green.png"];
+var w = window.innerWidth;
+var h = window.innerHeight;
+let holder = $("#vaquitas");
+
+
+function getRandomInt(min, max) {
+  return Math.floor(min + Math.random() * Math.floor(max - min));
+}
+
+function createNewVaquita(iter) {
+  w = window.innerWidth;
+  h = window.innerHeight;
+
+  let object = $("<img src = '" + srcs[iter % 3] + "'>");
+  object.css("position", "absolute");
+  object.css("width",  getRandomInt(100, 200) + "px");
+  object.css("top",  getRandomInt(h/4, 3*h/4) + "px");
+  object.css("left",  getRandomInt(0, 9*w/10) + "px");
+  object.css("transform", "rotate(" + getRandomInt(-90, 90) + ")");
+  object.css("transform", "scaleX(" + Math.pow(-1, iter) + ")");
+  holder.append(object);
+}
 
 function countUp(num, val) {
   let iter = 0;
+  holder.empty();
   for (let i = 0; i <= num; i += num/100) {
     setTimeout(function() {
       if (clickNum == val) {
@@ -15,6 +39,11 @@ function countUp(num, val) {
       $("#counter_bubble").html("<h1>" + Math.floor(num) + "<h1>");
     }
   }, 1000);
+
+  for (let i = 0; i <= num/10; ++i) {
+    createNewVaquita(i);
+  }
+
 }
 countUp(100);
 
